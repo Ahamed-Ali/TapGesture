58a3534d0d4a9f3f092dedb63726b68b29d18212
Ahamed-Ali <102580874+Ahamed-Ali@users.noreply.github.com>
2023-06-23 12:30:59 +0530

    Add files via upload
    TapGesture Sample

---
+++ TapRecognizerIssueSolution/TapRecognizerIssue/CustomControl.cs
@@ -0,0 +1,30 @@
+﻿using Syncfusion.Maui.Core.Internals;
+using Syncfusion.Maui.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TapRecognizerIssue
+{
+    internal class CustomParentBoxView : ContentView
+    {
+        public CustomParentBoxView()
+        {
+           
+            var absoluteLayout = new AbsoluteLayout();
+            Content = absoluteLayout;
+
+            var tapGestureRecognizer = new TapGestureRecognizer();
+            tapGestureRecognizer.Tapped += OnBoxTapped;
+            this.GestureRecognizers.Add(tapGestureRecognizer);
+        }
+
+        private void OnBoxTapped(object sender, EventArgs e)
+        {
+            BackgroundColor = BackgroundColor == Colors.Red ? Colors.Green : Colors.Red;
+        }
+    }
+
+}
